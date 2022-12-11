a91c6f62ee82142e082111343c4725d1268c9618
brndd <burneddi@gmail.com>
2022-12-11 04:40:27 +0200

    Initial commit

---
+++ convert.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s extglob
+
+escape_regex () {
+    printf %s "$1" | sed 's/[][()\.^$?*+]/\\&/g'
+}
+
+find_art () {
+    albumartist=$(printf '%s\n' "$1" | grep "Albumartist:" | sed -n "s/^.*Albumartist:\s*\(.*\)$/\1/p" )
+    albumartist=$(escape_regex "$albumartist")
+    albumname=$(printf '%s\n' "$1" | grep "Album:" | sed -n "s/^.*Album:\s*\(.*\)$/\1/p")
+    albumname=$(escape_regex "$albumname")
+    coverart=$(find "$2" -maxdepth 1 -regextype egrep -regex ".*/(cover|$albumartist.*$albumname)\.(jpe?g|png)" -print -quit)
+    printf '%s' "$coverart"
+}
+
+# Check dependencies
+if ! command -v metaflac &> /dev/null; then
+    echo "metaflac is not installed. Aborting."
+    exit 1
+fi
+if ! command -v opusenc &> /dev/null; then
+    echo "opusenc is not installed. Aborting."
+    exit 1
+fi
+if ! command -v convert &> /dev/null; then
+    echo "ImageMagick (convert) is not installed. Aborting."
+    exit 1
+fi
+if ! command -v parallel &> /dev/null; then
+    echo "GNU parallel is not installed. Aborting."
+    exit 1
+fi
+
+#Check params
+if [[ "$#" -lt 2 ]]; then
+    echo "Error: too few parameters."
+    exit 1
+fi
+if [[ "$#" -gt 2 ]]; then
+    echo "Error: too many parameters."
+    exit 1
+fi
+flac_dir=$(realpath "$1")
+if [[ ! -e "$flac_dir" ]]; then
+    echo "Input directory not found: $1"
+    exit 1
+fi
+
+opus_dir=$(realpath "$2")
+temp_dir="/tmp/musicconvert"
+if [[ -e "$temp_dir" ]]; then
+    echo "Temporary directory \(${temp_dir}\) already exists, exiting."
+    exit 1
+fi
+
+file_count=$(find "$flac_dir" -name "*.flac" | wc -l)
+echo "This will convert $file_count FLACs from $flac_dir into $opus_dir."
+read -p "Are you sure? (y/N) " -n 1 -r
+echo  
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Aborting."
+    exit 1
+fi
+
+find "$flac_dir" -name "*.flac" | while read flac_file; do
+    flac_file_noext="${flac_file%.*}"
+    flac_file_dir=$(dirname "$flac_file")
+    opus_file="$opus_dir/${flac_file_noext#$flac_dir/}.opus"
+    #If the transcoded file already exists, we don't need to do anything
+    if [[ -e "$opus_file" ]]; then continue; fi
+
+    opus_file_dir=$(dirname "$opus_file")
+    mkdir -p "$opus_file_dir"
+    exif=$(exiftool -S "$flac_file")
+
+    coverart_path=""
+
+    if ! [[ $(printf '%s\n' "$exif" | grep "Picture:") ]]; then
+        #If there is no embedded album art, check if there's a (cover|albumartist.?album).(jpe?g|png)
+        coverart=$(find_art "$exif" "$flac_file_dir")
+        if [[ -n "$coverart" ]]; then
+            #Some releases include 6MB cover art, we don't need good quality so convert it to be sub 50KB
+            coverart_noext="${coverart%.*}"
+            new_cover_file="${opus_file_dir}/cover.jpg"
+            #Create new file alongside output opuses if it doesn't exist already
+            if [[ ! -e "$new_cover_file" ]]; then
+                convert -define jpeg:extent=100KB -resize 500x500\> "$coverart" "$new_cover_file"
+            fi
+            #Embed the file
+            coverart_path="$new_cover_file"
+        fi
+    else
+        #If there is embedded album art, make it smaller if it's large (>100 KB), and extract it to be next to the files if needed.
+        coverart=$(find_art "$exif" "$flac_file_dir")
+        coverart_noext="${coverart%.*}"
+        new_cover_file="${opus_file_dir}/cover.jpg"
+        if [[ -n "$coverart" ]]; then
+            #We found existing coverart; let's imagemagick that to be nice and small & copy it next to the opus files
+            if [[ ! -e "$new_cover_file" ]]; then
+                convert -define jpeg:extent=100KB -resize 500x500\> "$coverart" "$new_cover_file"
+            fi
+            #no need to embed it here since there is embedded art already
+        fi
+        #Resize the embedded cover art if it's >100KB
+        artsize=$(printf '%s\n' "$exif" | grep "PictureLength:" | sed -n "s/^.*PictureLength:\s*\(.*\)$/\1/p" )
+        #We'll extract it no matter what, this way we can wipe all art from the files and embed our own one
+        #(some small number of files may include multiple pictures, and we only want one really)
+        extracted_art_dir=$(dirname "$temp_dir/${flac_file#$flac_dir/}")
+        extracted_art_file="$extracted_art_dir/cover.jpg"
+        converted_art_file="$extracted_art_dir/small.jpg"
+        mkdir -p "$extracted_art_dir"
+        metaflac --export-picture-to="$extracted_art_file" "$flac_file"
+        if (( artsize > 100000 )); then # Gyaa~! It's so big~!
+            convert -define jpeg:extent=100KB -resize 500x500\> "$extracted_art_file" "$converted_art_file"
+            
+            #Stick the converted file in the dir if there's no cover yet
+            if [[ ! -e "$new_cover_file" ]]; then
+                cp "$converted_art_file" "$new_cover_file"
+            fi
+        fi
+        #Finally, embed the file
+        coverart_path="$converted_art_file"
+    fi
+    if [[ -n "$coverart_path" ]]; then
+        printf '%s\0' "--picture"
+        printf '%s\0' "$coverart_path"
+    else
+        printf ''
+        printf ''
+    fi 
+    printf '%s\0' "$flac_file"
+    printf '%s\0' "$opus_file"
+done |
+parallel --bar --trim lr -N 4 -0 opusenc --quiet --discard-pictures --bitrate 96 --downmix-stereo {}
+
+rm -rf "/tmp/musicconvert"
